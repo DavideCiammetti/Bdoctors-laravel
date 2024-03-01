cede664742fbedc558355ab1c5f7f656d8d4287d
MatteoSanson <matteo.sanson@gmail.com>
2024-03-01 18:16:59 +0100

    feat: create doctors

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Laravel Auth Template
 
 1) [x] migrations surname in users più aggiunta nel campo form
-2) [] migrations per tabella doctors, model e resource controller
+2) [x] migrations per tabella doctors e model
 3) [] relazione con tabella users
 4) [] migrations specializations con relazione doctors, relativo model e seeders 
-5) [] CRUD doctors
+5) [] resource controller e CRUD doctors
 6) [] validazioni FE-BE
---
+++ database/migrations/2024_03_01_165508_create_doctors_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug',100)->nullable();
+            $table->string('address',100);
+            $table->string('phone_number',100)->nullable();
+            $table->text('doctor_img')->nullable();
+            $table->text('doctor_cv')->nullable();
+            $table->boolean('is_available')->default(1)->nullable();
+            $table->text('services')->nullable();
+            // $table->unsignedBigInteger('user_id')->after('id');
+            // $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doctors');
+    }
+};
