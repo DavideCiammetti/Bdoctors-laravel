880b0e22950ef10d5e1668420f63784d8b01eb29
DavideCiammetti <davideciammetti97@gmail.com>
2024-03-04 15:55:58 +0100

    feat: tabels sponsorship, messages

---
+++ database/migrations/2024_03_04_124825_create_messages.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 30)->nullable();
+            $table->string('surname', 40)->nullable();
+            $table->string('email', 80)->required();
+            $table->string('phone_number',15)->nullable();
+            $table->text('message')->required();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};
