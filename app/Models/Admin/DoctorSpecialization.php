7d3d37b351eb5c3655625c61e9f6259aba4e034c
Luigi-Iorio <luigi0iorio@gmail.com>
2024-03-06 13:06:43 +0100

    feat: Doctor-Specialization model

---
+++ app/Models/Admin/DoctorSpecialization.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models\Admin;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DoctorSpecialization extends Model
+{
+    use HasFactory;
+
+    // Specifica il nome della tabella se non segue la convenzione di denominazione predefinita
+    protected $table = 'doctor_specialization';
+
+    // Specifica che la chiave primaria è composta da due colonne
+    protected $primaryKey = ['doctor_id', 'specialization_id'];
+
+    // Disabilita l'incremento automatico per la chiave primaria
+    public $incrementing = false;
+
+    // Specifica le colonne che possono essere riempite con il metodo create
+    protected $fillable = [
+        'doctor_id',
+        'specialization_id',
+    ];
+}
