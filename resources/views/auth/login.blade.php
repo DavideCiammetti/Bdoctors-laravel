0f4b45de2494eb0b4d4fe7a8cb9240641b6be5ac
DavideCiammetti <davideciammetti97@gmail.com>
2024-03-08 10:49:52 +0100

    feat: modify language

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Login') }}</div>
+                    <div class="card-header">{{ __('Accedi') }}</div>
 
                     <div class="card-body">
                         <form method="POST" action="{{ route('login') }}">
@@ -13,7 +13,7 @@
 
                             <div class="mb-4 row">
                                 <label for="email"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                                    class="col-md-4 col-form-label text-md-right">{{ __('Indirizzo E-mail') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="email" type="email"
@@ -52,7 +52,7 @@
                                             {{ old('remember') ? 'checked' : '' }}>
 
                                         <label class="form-check-label" for="remember">
-                                            {{ __('Remember Me') }}
+                                            {{ __('Resta collegato') }}
                                         </label>
                                     </div>
                                 </div>
@@ -61,12 +61,12 @@
                             <div class="mb-4 row mb-0">
                                 <div class="col-md-8 offset-md-4">
                                     <button type="submit" class="send btn btn-primary">
-                                        {{ __('Login') }}
+                                        {{ __('Accedi') }}
                                     </button>
 
                                     @if (Route::has('password.request'))
                                         <a class="btn btn-link" href="{{ route('password.request') }}">
-                                            {{ __('Forgot Your Password?') }}
+                                            {{ __('Password dimenticata?') }}
                                         </a>
                                     @endif
                                 </div>
