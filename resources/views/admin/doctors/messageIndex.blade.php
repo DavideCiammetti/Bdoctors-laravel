d7bd88e9f8addc9b2ec1642a1bce3c98c2fac7d8
CaldatoLuca <lucacaldato15@gmail.com>
2024-03-11 18:50:02 +0100

    feat: style table

--- resources/views/admin/doctors/messageIndex.blade.php
+++ resources/views/admin/doctors/messageIndex.blade.php
@@ -8,17 +8,17 @@
             <table class="table">
                 <thead>
                     <tr>
-                        <th scope="col"></th>
-                        <th scope="col">Messaggio</th>
-                        <th scope="col">Cellulare</th>
-                        <th scope="col">E-mail</th>
+                        <th scope="col" class="bg-green-dark"></th>
+                        <th scope="col" class="bg-green-dark">Messaggio</th>
+                        <th scope="col" class="bg-green-dark">Cellulare</th>
+                        <th scope="col" class="bg-green-dark">E-mail</th>
 
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($messages as $message)
                         <tr>
-                            <th scope="row">
+                            <th scope="row" class="bg-green-light">
                                 @if ($message->name && $message->surname)
                                     {{ $message->name }} {{ $message->surname }}
                                 @else
--- resources/views/admin/doctors/reviewsIndex.blade.php
+++ resources/views/admin/doctors/reviewsIndex.blade.php
@@ -1,24 +1,24 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div id="messages" class="container pt-5">
-        <h1 class="mb-4">Lista Messaggi</h1>
+    <div id="reviews" class="container pt-5">
+        <h1 class="mb-4">Recensioni</h1>
 
         @if (count($user->doctor->messages) > 0)
             <table class="table">
                 <thead>
                     <tr>
-                        <th scope="col"></th>
-                        <th scope="col">Recensione</th>
-                        <th scope="col">Cellulare</th>
-                        <th scope="col">E-mail</th>
+                        <th scope="col" class="bg-green-dark"></th>
+                        <th scope="col" class="bg-green-dark">Recensione</th>
+                        <th scope="col" class="bg-green-dark">Cellulare</th>
+                        <th scope="col" class="bg-green-dark">E-mail</th>
 
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($reviews as $review)
                         <tr>
-                            <th scope="row">
+                            <th scope="row" class="bg-green-light">
                                 @if ($review->name && $review->surname)
                                     {{ $review->name }} {{ $review->surname }}
                                 @else
