05ce49022a12e6103a2b34e58fd7e5f9e5e0eb44
CaldatoLuca <lucacaldato15@gmail.com>
2024-03-13 11:31:42 +0100

    feat: mex e rec resp

--- resources/views/admin/doctors/reviewsIndex.blade.php
+++ resources/views/admin/doctors/reviewsIndex.blade.php
@@ -6,44 +6,63 @@
 
         @if (count($user->doctor->reviews) > 0)
             <div class="accordion accordion-flush" id="accordionFlushExample">
-                @foreach ($reviews as $review)
-                    <div class="accordion-item">
+                @foreach ($reviews as $key => $review)
+                    @if ($key + 1 <= 5)
+                        <div class="accordion-item">
+                            <h6 class="accordion-header d-flex justify-content-between align-items-center bg-green-dark ">
+                                <div class="d-flex justify-content-lg-between justify-content-around w-100 px-4">
+                                    {{-- Nome e cognome  --}}
+                                    <div class="w-25">
+                                        @if ($review->name && $review->surname)
+                                            {{ $review->name }} {{ $review->surname }}
+                                        @else
+                                            Anonimo
+                                        @endif
+                                    </div>
 
-                        <h6 class="accordion-header d-flex justify-content-between align-items-center bg-green-dark ">
-                            <div class="d-flex justify-content-between w-100 px-4">
-                                {{-- Nome e cognome meaaggio --}}
-                                <div class="w-25">{{ $review->name }} {{ $review->surname }}</div>
+                                    {{-- Data di invio --}}
+                                    <div class="w-25">
+                                        {{ str_replace(array_keys($italianMonths), array_values($italianMonths), $review->created_at->isoFormat('DD MMMM YYYY', 'it')) }}
+                                    </div>
 
-                                {{-- Data di invio --}}
-                                <div class="w-25">
-                                    {{ str_replace(array_keys($italianMonths), array_values($italianMonths), $review->created_at->isoFormat('DD MMMM YYYY', 'it')) }}
+                                    {{-- Numero di telefoni --}}
+                                    <div class="w-25 d-none d-lg-block">
+                                        {{ $review->phone_number }}
+                                    </div>
+
+                                    {{-- Email --}}
+                                    <div class="w-25 d-none d-lg-block">
+                                        {{ $review->email }}
+                                    </div>
                                 </div>
+                                <button class="btn collapsed text-white " type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#flush-collapseThree{{ $review->id }}" aria-expanded="false"
+                                    aria-controls="flush-collapseThree">
+                                    <i class="fa-solid fa-chevron-down"></i>
+                                </button>
 
-                                <div class="w-25">
-                                    {{ $review->phone_number }}
-                                </div>
+                            </h6>
+                            <div id="flush-collapseThree{{ $review->id }}" class="accordion-collapse collapse "
+                                data-bs-parent="#accordionFlushExample" style="">
+                                <div class="accordion-body">
+                                    {{ $review->content }}
+                                    {{-- Numero di telefoni --}}
+                                    <div class="d-block d-lg-none">
+                                        Telefono: {{ $review?->phone_number ?: '-' }}
+                                    </div>
 
-
-                                <div class="w-25">
-                                    {{ $review->email }}
+                                    {{-- Email --}}
+                                    <div class="d-block d-lg-none">
+                                        Email: {{ $review?->email ?: '-' }}
+                                    </div>
                                 </div>
                             </div>
-                            <button class="btn collapsed text-white " type="button" data-bs-toggle="collapse"
-                                data-bs-target="#flush-collapseThree{{ $review->id }}" aria-expanded="false"
-                                aria-controls="flush-collapseThree">
-                                <i class="fa-solid fa-chevron-down"></i>
-                            </button>
-
-                        </h6>
-                        <div id="flush-collapseThree{{ $review->id }}" class="accordion-collapse collapse "
-                            data-bs-parent="#accordionFlushExample" style="">
-                            <div class="accordion-body">{{ $review->content }}</div>
                         </div>
-                    </div>
+                    @endif
                 @endforeach
             </div>
         @else
-            <h3>Nessuna Recensione</h3>
+            <h3>Nessuno Messaggio ricevuto</h3>
         @endif
 
     </div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -133,7 +133,6 @@
                         </div>
                     </div>
                     <div class="card-body">
-
                         @if (count($user->doctor->messages) > 0)
                             <div class="accordion accordion-flush" id="accordionFlushExample">
                                 @foreach ($messages as $key => $message)
@@ -141,21 +140,28 @@
                                         <div class="accordion-item">
                                             <h6
                                                 class="accordion-header d-flex justify-content-between align-items-center bg-green-dark ">
-                                                <div class="d-flex justify-content-between w-100 px-4">
+                                                <div
+                                                    class="d-flex justify-content-lg-between justify-content-around w-100 px-4">
                                                     {{-- Nome e cognome meaaggio --}}
-                                                    <div class="w-25">{{ $message->name }} {{ $message->surname }}</div>
+                                                    {{-- Nome e cognome  --}}
+                                                    <div class="w-25">
+                                                        @if ($message->name && $message->surname)
+                                                            {{ $message->name }} {{ $message->surname }}
+                                                        @else
+                                                            Anonimo
+                                                        @endif
+                                                    </div>
 
                                                     {{-- Data di invio --}}
-                                                    <div class="w-25">
+                                                    <div class="w-25 text-center">
                                                         {{ str_replace(array_keys($italianMonths), array_values($italianMonths), $message->created_at->isoFormat('DD MMMM YYYY', 'it')) }}
                                                     </div>
 
-                                                    <div class="w-25">
+                                                    <div class="w-25 text-center d-none d-lg-block">
                                                         {{ $message->phone_number }}
                                                     </div>
 
-
-                                                    <div class="w-25">
+                                                    <div class="w-25 text-center d-none d-lg-block">
                                                         {{ $message->email }}
                                                     </div>
                                                 </div>
@@ -170,14 +176,25 @@
                                             <div id="flush-collapseThree{{ $message->id }}"
                                                 class="accordion-collapse collapse " data-bs-parent="#accordionFlushExample"
                                                 style="">
-                                                <div class="accordion-body">{{ $message->message }}</div>
+                                                <div class="accordion-body">
+                                                    {{ $message->message }}
+                                                    {{-- Numero di telefoni --}}
+                                                    <div class="d-block d-lg-none">
+                                                        Telefono: {{ $message?->phone_number ?: '-' }}
+                                                    </div>
+
+                                                    {{-- Email --}}
+                                                    <div class="d-block d-lg-none">
+                                                        Email: {{ $message?->email ?: '-' }}
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                     @endif
                                 @endforeach
                             </div>
                         @else
-                            <h4>Nessuno Messaggio ricevuto</h4>
+                            <h3>Nessuno Messaggio ricevuto</h3>
                         @endif
                     </div>
                 </div>
@@ -196,7 +213,6 @@
                         </div>
                     </div>
                     <div class="card-body">
-
                         @if (count($user->doctor->reviews) > 0)
                             <div class="accordion accordion-flush" id="accordionFlushExample">
                                 @foreach ($reviews as $key => $review)
@@ -204,21 +220,29 @@
                                         <div class="accordion-item">
                                             <h6
                                                 class="accordion-header d-flex justify-content-between align-items-center bg-green-dark ">
-                                                <div class="d-flex justify-content-between w-100 px-4">
-                                                    {{-- Nome e cognome meaaggio --}}
-                                                    <div class="w-25">{{ $review->name }} {{ $review->surname }}</div>
+                                                <div
+                                                    class="d-flex justify-content-lg-between justify-content-around w-100 px-4">
+                                                    {{-- Nome e cognome  --}}
+                                                    <div class="w-25">
+                                                        @if ($review->name && $review->surname)
+                                                            {{ $review->name }} {{ $review->surname }}
+                                                        @else
+                                                            Anonimo
+                                                        @endif
+                                                    </div>
 
                                                     {{-- Data di invio --}}
                                                     <div class="w-25">
                                                         {{ str_replace(array_keys($italianMonths), array_values($italianMonths), $review->created_at->isoFormat('DD MMMM YYYY', 'it')) }}
                                                     </div>
 
-                                                    <div class="w-25">
+                                                    {{-- Numero di telefoni --}}
+                                                    <div class="w-25 d-none d-lg-block">
                                                         {{ $review->phone_number }}
                                                     </div>
 
-
-                                                    <div class="w-25">
+                                                    {{-- Email --}}
+                                                    <div class="w-25 d-none d-lg-block">
                                                         {{ $review->email }}
                                                     </div>
                                                 </div>
@@ -233,14 +257,25 @@
                                             <div id="flush-collapseThree{{ $review->id }}"
                                                 class="accordion-collapse collapse " data-bs-parent="#accordionFlushExample"
                                                 style="">
-                                                <div class="accordion-body">{{ $review->content }}</div>
+                                                <div class="accordion-body">
+                                                    {{ $review->content }}
+                                                    {{-- Numero di telefoni --}}
+                                                    <div class="d-block d-lg-none">
+                                                        Telefono: {{ $review?->phone_number ?: '-' }}
+                                                    </div>
+
+                                                    {{-- Email --}}
+                                                    <div class="d-block d-lg-none">
+                                                        Email: {{ $review?->email ?: '-' }}
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                     @endif
                                 @endforeach
                             </div>
                         @else
-                            <h4>Nessuno Messaggio ricevuto</h4>
+                            <h3>Nessuno Messaggio ricevuto</h3>
                         @endif
                     </div>
                 </div>
